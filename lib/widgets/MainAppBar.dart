4004263ecde7092c9f59720ffdc515c2ef5735e4
NickBonet <nbonet@kshadow.us>
2020-04-15 19:58:39 -0400

    Another win for global loginState!

--- lib/widgets/MainAppBar.dart
+++ lib/widgets/MainAppBar.dart
@@ -1,4 +1,8 @@
 import "package:flutter/material.dart";
+import 'package:provider/provider.dart';
+import 'package:flutter_mobx/flutter_mobx.dart';
+
+import 'package:turnip_frontend/stores/LoginStore.dart';
 
 class MainAppBar extends StatefulWidget implements PreferredSizeWidget {
   MainAppBar({Key key, this.title}) : preferredSize = Size.fromHeight(kToolbarHeight), super(key : key);
@@ -20,25 +24,32 @@ class _MainAppBar extends State<MainAppBar> {
 
   @override
   Widget build(BuildContext context) {
-    return AppBar(
-      title: Row(
-          children: [
-            Image.asset('assets/images/leaf_icon.png', width:48, height: 48,),
-            SizedBox(width: 10,), // spacing between leaf and text!
-            Text(widget.title),
-          ],),
-      backgroundColor: Color(0xFF065c1b),
-      actions: <Widget>[
-          OutlineButton(
-            onPressed: () {  },
-            child: Text("Sign Up", style: TextStyle(color: Colors.white))),
-          OutlineButton(
-            onPressed: () {
-              Navigator.pushNamed(context, '/login');
-            },
-            child: Text("Login", style: TextStyle(color: Colors.white))  
-            ),
-        ],
+    final loginStore = Provider.of<LoginStore>(context, listen: false);
+    return Observer(
+        builder: (_) => AppBar(
+        title: Row(
+            children: [
+              Image.asset('assets/images/leaf_icon.png', width:48, height: 48,),
+              SizedBox(width: 10,), // spacing between leaf and text!
+              Text(widget.title),
+            ],),
+        backgroundColor: Color(0xFF065c1b),
+        actions: <Widget>[
+            OutlineButton(
+              onPressed: () {  },
+              child: Text("Sign Up", style: TextStyle(color: Colors.white))),
+            !loginStore.loggedIn ? OutlineButton(
+              onPressed: () {
+                Navigator.pushNamed(context, '/login');
+              },
+              child: Text("Login", style: TextStyle(color: Colors.white))  
+              )
+            : OutlineButton(
+              onPressed: () {},
+              child: Text('Hello there!', style: TextStyle(color: Colors.white))
+              )
+          ],
+      )
     );
   }
 }
